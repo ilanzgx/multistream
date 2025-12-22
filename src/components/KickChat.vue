87004837a6fd10b675e77037925b8ab117c7248c
ilanzgx <ilan-_@hotmail.com>
2025-12-22 16:01:40 -0300

    feat: kick iframes components

---
+++ src/components/KickChat.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+defineProps<{ channel: string }>();
+</script>
+
+<template>
+  <iframe :src="`https://kick.com/${channel}/chatroom`" frameborder="0" />
+</template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 80vh;
+}
+</style>
---
+++ src/components/KickStream.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ channel: string }>();
+</script>
+
+<template>
+  <iframe
+    :src="`https://player.kick.com/${channel}`"
+    allowfullscreen
+    frameborder="0"
+  />
+</template>
+
+<style scoped>
+iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
