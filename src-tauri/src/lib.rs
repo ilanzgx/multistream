3602e7ab4c0292940b7dd765f391aa0eed6ec4eb
ilanzgx <ilan-_@hotmail.com>
2026-02-26 18:41:23 -0300

    feat: add plugin_single_instance to prevent multiple windows

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,4 @@
+use tauri::Manager;
 
 // fixed port
 const LOCALHOST_PORT: u16 = 14831;
@@ -5,9 +6,23 @@ const LOCALHOST_PORT: u16 = 14831;
 // realistic Chrome user-agent to avoid Cloudflare WebView fingerprint detection
 const USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36";
 
+// this works on Windows, Linux and macOS
+// dont touch this code, dont change it unless you know what you are doing
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+    // load plugin_single_instance
+    #[cfg(desktop)] {
+        builder = builder.plugin(tauri_plugin_single_instance::init(|app, _args, _cwd| {
+            if let Some(window) = app.get_webview_window("main") {
+                let _ = window.set_focus();
+            }
+        }));
+    }
+
+    // load plugin_http, plugin_updater, plugin_process, plugin_localhost
+    builder
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_process::init())
@@ -21,7 +36,7 @@ pub fn run() {
                 )?;
             }
 
-            // create the window manually so can set user_agent
+            // resolve url
             let url = if cfg!(debug_assertions) {
                 tauri::WebviewUrl::External("http://localhost:5173".parse().unwrap())
             } else {
@@ -32,6 +47,7 @@ pub fn run() {
                 )
             };
 
+            // create the window manually so can set user_agent
             tauri::WebviewWindowBuilder::new(app, "main", url)
                 .title("Multistream")
                 .inner_size(1280.0, 720.0)
