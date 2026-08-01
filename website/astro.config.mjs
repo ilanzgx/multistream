ab27313f8cdbda163b71a6323013f09cb7604717
ilanzgx <ilan-_@hotmail.com>
2026-08-01 16:53:13 -0300

    feat(website): optimize seo, i18n and changelog parsing

--- website/astro.config.mjs
+++ website/astro.config.mjs
@@ -2,10 +2,13 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://usemultistream.vercel.app',
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+  integrations: [sitemap()]
 });
